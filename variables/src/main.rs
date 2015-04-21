db908633da26ccbcba6eb97cb446e2f5c73b70b4
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-04-21 23:44:02 +0300

    Add Rust variables

---
+++ variables/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+
+    let (x, y) = (1, 2); // type inference to i32 / u - unsigned
+
+    let z = 5;
+        // z = 3; immutable
+
+    let mut G = 292;
+    G = 29;
+
+    println!("Mutated G {}", G);
+}
